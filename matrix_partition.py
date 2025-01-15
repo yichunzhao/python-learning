545a37810adb345ad1a5e4f8a1dc9088da8000be
Yichun Zhao <zhaoyichun@hotmail.com>
2025-01-15 23:35:34 +0100

    init zero matrix

--- matrix_partition.py
+++ matrix_partition.py
@@ -7,3 +7,14 @@ a = np.random.randint(1, 100, (4, 20))
 print(a)
 print("a shape: " + str(a.shape))
 print("a partition: " + str(a[:, (2, 4)]))
+
+# create a (4,10) matrix
+b = np.random.randint(1, 100, (4, 1))
+# print out b with description string
+print("b: " + str(b))
+
+# create matrix zeros_like b
+c = np.zeros_like(b)
+# print out c with description string
+print("c: " + str(c))
+
